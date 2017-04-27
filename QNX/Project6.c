ddc7255d1b5ce94c58195bdb4de477526834f44d
dmacintyre <djm4912@rit.edu>
2017-04-27 19:02:31 -0400

    Adding QNX and STM32 folder structure

---
+++ QNX/Project6.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <sys/neutrino.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <hw/inout.h>
+
+#define QNX_BASE_ADDRESS (0x280)
+
+#define ADC_MAX (16666) // 5V * 305 uV/bit (resolution)
+#define ADC_MIN (-16666)
+
+float normalizeADCVal(int adcVal) {
+	return (float)(adcVal - ADC_MIN)/(float)(ADC_MAX-ADC_MIN);
+}
+
+int main(int argc, char *argv[]) {
+
+	int16_t lsb, msb;
+	int16_t adcVal;
+
+	printf("Welcome to the hello QNX Momentics IDE\n");
+
+
+	// Get access to hardware IO
+    if ( ThreadCtl(_NTO_TCTL_IO, NULL) == -1)
+    {
+
+    	printf("Failed to get I/O access permission");
+    }
+
+    // PORTA is located at QNX_BASE_ADDRESS + 8 (register 0x288)
+ //   port_a = mmap_device_io(1, QNX_BASE_ADDRESS + 8);
+ //   dir = mmap_device_io(1, QNX_BASE_ADDRESS + 11);
+
+	// Set channel range to fixed channel 0
+	out8(QNX_BASE_ADDRESS + 2, 0x00);
+
+	printf("Fixed channel\n");
+
+	// Select the input range
+	// Use a gain of 1 +10V to -10V
+	out8(QNX_BASE_ADDRESS + 3,0x00);
+
+	// Select the Polarity
+	// Set to page 2
+	out8(QNX_BASE_ADDRESS + 1, 2);
+	// Set bi-polar, single ended
+	out8(QNX_BASE_ADDRESS+13, 0x00);
+
+	printf("Polling anlog input %u\n");
+
+	// Wait for analog input circuit to settle
+	while(in8(QNX_BASE_ADDRESS + 3) & 0x20);
+
+	// Perform a conversion on the current channel
+	out8(QNX_BASE_ADDRESS, 0x80);
+
+	printf("Polling convesion finish %u\n");
+
+	// Poll the status flag for the conversion to finish
+	while (in8(QNX_BASE_ADDRESS + 3) & 0x80);
+	lsb = in8(QNX_BASE_ADDRESS);
+	msb = in8(QNX_BASE_ADDRESS+1);
+	printf("ADC reading 0x%x\n", lsb);
+	printf("ADC reading 0x%x\n", msb);
+	adcVal = lsb | (msb << 8);
+	printf("Reading: %d\n", adcVal);
+	printf("Voltage: %f\n", adcVal * 0.0003);
+	printf("Norm val %f\n", normalizeADCVal(adcVal));
+
+	return EXIT_SUCCESS;
+}
